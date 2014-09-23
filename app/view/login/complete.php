58ce527b7956cde102d55f4f9c35129f367857d2
gcheval <chevalier321@gmail.com>
2014-09-23 13:08:15 -0400

    fix redirect link

--- app/view/login/complete.php
+++ app/view/login/complete.php
@@ -1,3 +1,3 @@
 Login terminée. (E-mail : <?php echo $user->getMail(); ?>)<br/>
 Vous êtes présentement connecté. Vous serrez redirigé dans 3 secondes.
-<?php header('Refresh: 3; URL='); ?>
+<?php header('Refresh: 3; URL=/log210'); ?>
