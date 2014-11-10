9173e9a2d4a3db3d912839637ada01bcc3f88746
gcheval <chevalier321@gmail.com>
2014-11-10 17:09:02 -0500

    waiting for menus

--- app/view/restaurant/see.php
+++ app/view/restaurant/see.php
@@ -6,41 +6,48 @@ $menuItems = array(
     array("item2", "5.51"),
     array("item3", "5.52"),
     array("item4", "5.53"));
-?><br/>
-
-
-<div class="form-group">
-    <label for="menuItems">Items</label><br/>
-
-<?php foreach ($menuItems as $menuItem): ?> 
+if($restaurant->hasMenu()){
+    $menuItemsTest = $restaurant->getMenu()->getMenuItems();  
+    echo "got menu.";
+    
+     foreach ($menuItems as $menuItem): ?> 
         <div class="panel panel-default">
             <div class="panel-body">
                 <div class="row">  
                     
                     
                         <div class="text-left">
-                            <?php echo $menuItem[0]; ?>
+                            <?php echo $menuItem->getId(), " : " ,$menuItem->getName(); ?>
                         </div>
                         <div class="text-right">
-    Prix : <?php echo $menuItem[1]; ?>
+                            Prix : <?php echo $menuItem->getPrice(); ?>
                         </div>
                     <div class="col-sm-offset-9">
                         <div class="input-group">
 
-                            <input type="number" class="form-control" value="0">
+                            <input type="number" class="form-control" min="0" value="0" >
                             <span class="input-group-addon">Quantité</span>
                         </div><!-- /input-group -->
                     </div>
                 </div><!-- /.row -->
             </div></div>
-<?php endforeach; ?>
+    $menuItemQty = $menuItem->getId() => "0" ;
+<?php endforeach;
+}else{
+    echo "restaurant has no menu";
+}
+$menuItemQty = array();
+?><br/>
+
+
+<div class="form-group">
+    <label for="menuItems">Items</label><br/>
+
+
 </div>
 <div class="btn">
-    <button   onclick="myFunction()" class="btn btn-primary">Ajouter au panier</button>
+    <button   onclick="addToOrder()" class="btn btn-primary">Rafraîchir la commande</button>
 </div>
-
-<script>
-    function myFunction() {
-        alert("Les items sélectionnés ont étés ajoutés au panier.");
-    }
-</script>
+<div class="btn">
+    <button   onclick="processOrder()" class="btn btn-primary">Commander</button>
+</div>
